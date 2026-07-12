9de274a46919d0234ed2b897620383cfea57a300
music <147973498+music029@users.noreply.github.com>
2026-07-12 14:38:00 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,10 +1,13 @@
 import json
 import urllib.request
+import os
 
 
+# 原始配置地址
 SOURCE_URL = "https://9280.kstore.vip/wex.json"
 
 
+# 本地文件
 SOURCE_FILE = "fish.json"
 
 CONFIG_FILE = "config.json"
@@ -17,12 +20,15 @@ def download_source():
 
     try:
 
+        print("正在下载最新配置...")
+
         urllib.request.urlretrieve(
             SOURCE_URL,
             SOURCE_FILE
         )
 
-        print("配置更新成功")
+        print("下载完成:", SOURCE_FILE)
+
 
     except Exception as e:
 
@@ -30,6 +36,8 @@ def download_source():
             "下载失败:",
             e
         )
+
+        exit()
 
 
 
@@ -65,27 +73,68 @@ def save_json(file,data):
 def filter_sites(data,config):
 
 
-    remove = set(
-        config["remove_sites"]
+    remove_sites = set(
+        config.get(
+            "remove_sites",
+            []
+        )
     )
 
 
-    sites=[]
+    rename_sites = config.get(
+        "rename_sites",
+        {}
+    )
 
 
-    for item in data["sites"]:
+    new_sites = []
 
 
-        if item["key"] in remove:
+    for site in data.get(
+        "sites",
+        []
+    ):
+
+
+        key = site.get(
+            "key",
+            ""
+        )
+
+
+        name = site.get(
+            "name",
+            ""
+        )
+
+
+        # 删除站点
+        if (
+            key in remove_sites
+            or
+            name in remove_sites
+        ):
+
+            print(
+                "删除:",
+                name
+            )
 
             continue
 
 
-        sites.append(item)
+
+        # 修改名称
+        if key in rename_sites:
+
+            site["name"] = rename_sites[key]
+
+
+        new_sites.append(site)
 
 
 
-    data["sites"]=sites
+    data["sites"] = new_sites
 
 
     return data
@@ -95,24 +144,38 @@ def filter_sites(data,config):
 def main():
 
 
+    # 1.下载最新配置
+
     download_source()
 
 
-    config=load_json(
+
+    # 2.读取规则
+
+    config = load_json(
         CONFIG_FILE
     )
 
 
-    data=load_json(
+
+    # 3.读取源配置
+
+    data = load_json(
         SOURCE_FILE
     )
 
 
-    result=filter_sites(
+
+    # 4.过滤
+
+    result = filter_sites(
         data,
         config
     )
 
+
+
+    # 5.输出
 
     save_json(
         OUTPUT_FILE,
@@ -120,13 +183,30 @@ def main():
     )
 
 
+
+    print("===================")
+
     print(
-        "完成生成:",
+        "过滤完成"
+    )
+
+    print(
+        "输出文件:",
         OUTPUT_FILE
+    )
+
+    print(
+        "剩余站点:",
+        len(
+            result.get(
+                "sites",
+                []
+            )
+        )
     )
 
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     main()
